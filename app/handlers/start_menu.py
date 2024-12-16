809a9243c176bc6daa4b1c23c95eb579ea72797c
Gleb <shchebetko@gmail.com>
2024-12-16 10:26:42 +0500

    Add default middleware folder, add sqlal+alembic

--- app/handlers/start_menu.py
+++ app/handlers/start_menu.py
@@ -5,7 +5,10 @@ from keyboards.default_menu import get_start_menu_kb
 
 router = Router()
 
+
 @router.message(CommandStart())
 async def start_keyboard(message: Message):
     markup = get_start_menu_kb()
-    await message.answer("Привет тьютор. Что хочешь сделать?", reply_markup=markup.as_markup())
+    await message.answer(
+        "Привет тьютор. Что хочешь сделать?", reply_markup=markup.as_markup()
+    )
