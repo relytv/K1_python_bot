7a314d816718f81736937a5eaaa014f66227dd84
Gleb <shchebetko@gmail.com>
2024-12-16 12:36:34 +0500

    Add session in main()

--- app/main.py
+++ app/main.py
@@ -2,16 +2,22 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from handlers.start_menu import router as start_menu_router
+from handlers import start_menu
+
 
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
+    
+    engine = create_async_engine(url= "postgresql+asyncpg://postgres:postgres@localhost:9876/postgres", echo=True)
+    session = async_sessionmaker(engine, expire_on_commit=False)
+    
     bot = Bot("6963651623:AAEVd6CKaeP1vJKXT24WoY5DECa_tET6S7g")
     dp = Dispatcher()
-    dp.include_router(start_menu_router)
+    dp.include_router(start_menu.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
