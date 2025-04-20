d11c8a0dcaba13d7ac84d3cd38559f6602e77d82
Gleb <shchebetko@gmail.com>
2025-04-20 21:34:08 +0500

    Change order of routers and middlewares

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from app.middlewares.admin import AdminCheckMiddleware
 from app.middlewares.db import DatabaseMiddleware
-from app.handlers import start_menu, superadmin_commands
+from app.middlewares.superadmin import SuperAdminCheckMiddleware
+from app.handlers import start_menu, admin_commands, superadmin_commands, user_commands
 
 
 
@@ -17,19 +18,21 @@ async def main():
     engine = create_async_engine(url= "postgresql+asyncpg://k1_admin:postgres@localhost:9877/postgres", echo=True)
     session = async_sessionmaker(engine, expire_on_commit=False)
     
-    bot = Bot("6963651623:AAEVd6CKaeP1vJKXT24WoY5DECa_tET6S7g")
+    bot = Bot("8167371781:AAGZghp-nlyyNhy5gfAWPqkemUlGX-CvPcU")
     dp = Dispatcher()
-    dp.include_routers(start_menu.router, superadmin_commands.router)
+    dp.include_routers(start_menu.router, user_commands.router, admin_commands.router , superadmin_commands.router)
     
 
     dp.update.middleware(DatabaseMiddleware(session))
 
-    admin_router = superadmin_commands.router
-    
+    super_admin_router = superadmin_commands.router
+    admin_router = admin_commands.router
+
     admin_router.message.middleware(AdminCheckMiddleware(session))
     admin_router.callback_query.middleware(AdminCheckMiddleware(session))
-    
-    
+
+    super_admin_router.message.middleware(SuperAdminCheckMiddleware(session))
+    super_admin_router.callback_query.middleware(SuperAdminCheckMiddleware(session))
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
