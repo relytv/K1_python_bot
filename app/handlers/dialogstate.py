4558564a539b157a4720b35e611eade8b3791bab
Gleb <shchebetko@gmail.com>
2025-04-21 16:58:11 +0500

    New fsm state

--- app/handlers/dialogstate.py
+++ app/handlers/dialogstate.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 class LinkAccount(StatesGroup):
     waiting_for_name = State()
+
+class WaitGrades(StatesGroup):
+    waiting_for_grading = State()
