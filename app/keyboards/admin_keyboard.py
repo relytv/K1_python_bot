37b85d2be50837df349d4bbb70a3560e2253579f
Gleb <shchebetko@gmail.com>
2025-04-21 16:58:46 +0500

    Add new kb for admins

--- app/keyboards/admin_keyboard.py
+++ app/keyboards/admin_keyboard.py
@@ -1,11 +1,43 @@
-from aiogram import types
-from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardMarkup
+import uuid
+from aiogram.types import InlineKeyboardButton, KeyboardButton, ReplyKeyboardRemove
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.filters.callback_data import CallbackData
+
+class GroupsIdCallbackFactory(CallbackData, prefix="id",  sep="|"):
+
+    id: uuid.UUID
+    group: str
+
 
 
 def get_start_admin_menu_kb() -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
-    btn1 = types.InlineKeyboardButton(text="Показать группы", callback_data="show_my_groups")
-    btn2 = types.InlineKeyboardButton(text="Проставить оценки", callback_data="some_data")
+    btn1 = InlineKeyboardButton(text="Показать группы", callback_data="show_my_groups")
+    btn2 = InlineKeyboardButton(text="Проставить оценки", callback_data="give_rating")
     builder.add(btn1)
     builder.add(btn2)
     return builder
+
+
+def get_give_rating_kb(buttons_data: list[tuple[str, str, uuid.UUID]]) -> InlineKeyboardBuilder:
+    
+    builder = InlineKeyboardBuilder()
+    for group, location, id in buttons_data:
+        builder.button(
+            text=f"{group}: {location}", 
+            callback_data=GroupsIdCallbackFactory(
+               id=id,
+               group = group
+            )
+            )
+    builder.adjust(1)
+    return builder
+
+def get_grades_kb() -> ReplyKeyboardBuilder:
+
+    builder = ReplyKeyboardBuilder()
+    grade = 10
+    buttons: list[KeyboardButton] = [KeyboardButton(text=f"{i}") for i in range(10, 31, 5)]
+    builder.row(*buttons, width=3)
+    return builder
+    
